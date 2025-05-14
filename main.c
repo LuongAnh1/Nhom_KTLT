36a8f1aa2e5313d50d1020c5ac38d7554b447aca
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-05-14 22:02:08 +0700

    oke

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Data.h"
+
+extern Subject Table_Subjects[MAX_TABLE_SUBJECTS] = {NULL}; // Mảng danh sách môn học
+extern Student Table_Students[MAX_TABLE_STUDENTS] = {NULL}; // Mảng danh sách sinh viên
+int main(){
+    return 0;
+}
