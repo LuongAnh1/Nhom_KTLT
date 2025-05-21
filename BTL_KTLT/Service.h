2096806e1a108fc99cbf9ddcb5119a4be827512f
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-05-21 21:30:44 +0700

    cuong cu to

--- BTL_KTLT/Service.h
+++ BTL_KTLT/Service.h
@@ -1,13 +1,14 @@
 #ifndef SERVICE_H
 #define SERVICE_H
 #include "Data.h"
-
+//Cường
 // Xếp loại sinh viên
 void RankStudent(Student* Student);
 // Trung bình điểm của sinh viên (được gọi trong hàm InsertGrades)
 void AvgGrades(Student* Student);
 // Nhập điểm cho sinh viên
 void InsertGrades(Grades *Grades, char Student_id);
+//-------------------------------------------------------------
 // Sắp xếp danh sách sinh viên theo điểm trung bình
 void SortStudentByGPA();
 // Tìm kiếm sinh viên theo mã sinh viên
