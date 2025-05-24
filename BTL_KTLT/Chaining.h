3559d6e4db3c68cc7a058cee42c0037fff099d09
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-05-24 19:51:56 +0700

    thay đổi kiểu tra về của hàm Insert và Remove của Chaining về dạng Chaining*

--- BTL_KTLT/Chaining.h
+++ BTL_KTLT/Chaining.h
@@ -3,8 +3,8 @@
 #include "Data.h"
 
 Chaining* CreatNode(void* Data, void* Key);
-void Insert(Chaining* head, void* Data, void* Key);
+Chaining* Insert(Chaining* head, void* Data, void* Key);
 Chaining* Search(Chaining* head, void* Key);
-void Delete(Chaining* head, void* Key);
+Chaining* Delete(Chaining* head, void* Key);
 
 #endif
