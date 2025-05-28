b7866f763afc7a426199040049de4da478265f56
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-05-28 21:03:50 +0700

    ok

--- BTL_KTLT/test/test_chaining.c
+++ BTL_KTLT/test/test_chaining.c
@@ -1,18 +1,20 @@
 #include "Chaining.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // Kiểm thử hàm so sánh xâu
 void Test_CompareString (){
-    char *a = "ABCDEFG";
-    char *b = "abcdefg";
+    char *a = "abcdefg";
+    char *b = "abcdef";
     int k = CompareString(a,b);
     if (k <= 0) // tức xâu a <= xâu b
-        printf("Hàm CompareString của Chaining.h sai\n");
+        printf("Ham CompareString của Chaining.h sai\n");
     else
-        printf("Hàm CompareString của Chaining.h đúng\n");
+        printf("Ham CompareString cua Chaining.h dung\n");
 }
 
 int main(){
     Test_CompareString();
+    system("PAUSE");
     return 0;
 }
